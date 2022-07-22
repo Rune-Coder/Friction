96e76619c3d223d549285ccf4be83abe87b29c32
unknown <souhardya.dutta7801@gmail.com>
2022-07-22 14:05:24 +0530

    mark 7

--- src/advertise/advertise.js
+++ src/advertise/advertise.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import BadgeCheckIcon from '../icons/badgeCheckIcon';
+import HandLoveIcon from '../icons/handLoveIcon';
+import TruckIcon from '../icons/truckIcon';
+import GooglePlayIcon from '../icons/googlePlayIcon';
+import AppleIcon from '../icons/appleIcon';
 import classes from './advertise.module.css';
 
 function Advertise(props) {
     return(
         <div className={classes.advertise}>
             <div className={classes.characteristics}>
-                <div>Easy Exchange</div>
-                <div>100% Handpicked</div>
-                <div>Assured Quality</div>
+                <div><span className={classes.services}><TruckIcon /></span>Fast Delivery</div>
+                <div><span className={classes.services}><HandLoveIcon /></span>100% Handpicked</div>
+                <div><span className={classes.services}><BadgeCheckIcon /></span>Assured Quality</div>
             </div>
             <div className={classes.app}>
                 <p>Experience the shoe mobile app</p>
-                <button>Get it on Google Play</button>
-                <button>Download on the App Store</button>
+                <button className={classes.appBtn}>
+                    <span className={classes.play}>
+                        <GooglePlayIcon />
+                    </span>Get it on Google Play
+                </button>
+                <button className={classes.appBtn}>
+                    <span className={classes.play}>
+                        <AppleIcon />
+                    </span>Download on the App Store
+                </button>
             </div>
         </div>
     );
