5cca0d0cea110bf74ac7447afe82e762dfa05d51
unknown <souhardya.dutta7801@gmail.com>
2022-07-23 14:26:08 +0530

    mark 9

--- src/shoeMenu/shoeMenu.js
+++ src/shoeMenu/shoeMenu.js
@@ -3,12 +3,16 @@ import classes from './shoeMenu.module.css';
 
 function ShoeMenu(props) {
     return(
-        <div className={classes.menu}>
-            <ul>
-                <li>Men</li>
-                <li>Women</li>
-                <li>Kids</li>
-            </ul>
+        <div className={classes.shoeAccordian}>
+            <div className={classes.menu}>
+                <ul>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Kids</li>
+                    <li>Sports</li>
+                </ul>
+            </div>
+            <div className={classes.types}>hello</div>
         </div>
     );
 }
