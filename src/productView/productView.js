3d79c1650a215fedceae763e20d8e4e23763485a
unknown <souhardya.dutta7801@gmail.com>
2022-07-26 02:50:47 +0530

    mark 12

--- src/productView/productView.js
+++ src/productView/productView.js
@@ -6,14 +6,24 @@ function ProductView(props) {
         <div className={classes.view}>
             <div className={classes.image}>image</div>
             <div className={classes.details}>
-                <p>Company Name</p>
-                <p>Product Name</p>
+                <p className={classes.company}>Company Name</p>
+                <p className={classes.product}>Product Name</p>
                 <p>Rating</p>
-                <p>&#8377;x &#8377;y (z% off)</p>
-                <p>Inclusive of all taxes</p>
+                <p className={classes.price}>&#8377;x&nbsp;&nbsp;
+                    <span className={classes.mrp}>&#8377;y</span>
+                    <span className={classes.discount}>&nbsp;&nbsp;(z% off)</span>
+                </p>
+                <p className={classes.tax}>Inclusive of all taxes</p>
                 <p>SELECT SIZE (UK)</p>
-                <button>ADD TO BAG</button>
-                <button>WISHLIST</button>
+                <ul className={classes.size}>
+                    <li><button>6</button></li>
+                    <li><button>7</button></li>
+                    <li><button>8</button></li>
+                    <li><button>9</button></li>
+                    <li><button>10</button></li>
+                </ul>
+                <button className={classes.bag}>ADD TO BAG</button>&nbsp;
+                <button className={classes.wish}>WISHLIST</button>
             </div>
         </div>
     );
