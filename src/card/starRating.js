47c9d11e92f53eae43c2709e876dc463a192a19d
unknown <souhardya.dutta7801@gmail.com>
2022-07-27 03:04:37 +0530

    mark 13

--- src/card/starRating.js
+++ src/card/starRating.js
@@ -3,8 +3,7 @@ import StarIcon from '../icons/starIcon';
 import classes from './starRating.module.css';
 
 function StarRating(props) {
-    const fullStar = "3";
-    const partialStar = "5";
+    const fullStar = props.stars;
     return(
         <div className={classes.rating}>
             <span className={`${classes.star} ${fullStar >= 1 ? classes.starFill: ''}`}>
