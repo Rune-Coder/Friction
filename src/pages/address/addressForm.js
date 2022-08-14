f3f46035bcc5e6a3522bb2f26dc0ff4344d38c82
unknown <souhardya.dutta7801@gmail.com>
2022-08-15 03:35:00 +0530

    mark 27

--- src/pages/address/addressForm.js
+++ src/pages/address/addressForm.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './addressForm.module.css';
 
 function AddressForm(props){
+    const [mob, setMob] = useState(" ");
+    const [pin, setPin] = useState(" ");
+
     function addressSave(event){  
         event.preventDefault();
+
+        var ok = 0;
+        //Mobile no validation
+
+        if(mob.length < 10)
+            alert("Minimum length is 10");
+        else if(/^\d+$/.test(mob) === false)
+            alert("Please enter a valid 10 digit mobile number");
+        else if(mob.charAt(0) < 6)
+            alert("Please enter a valid 10 digit mobile number");
+        else
+            ok = 1;
+        if(ok === 0)
+            return;
+
+        //Pin code validation
+
+        if(pin.length < 6)
+            alert("Minimum length is 6");
+        else if(/^\d+$/.test(pin) === false)
+            alert("Only numbers are allowed");
+        else
+            ok = 1;
     }
+    function mobHandler(event){
+        const no = event.target.value;
+        setMob(no);    
+    }
+    function pinHandler(event){
+        const code = event.target.value;
+        setPin(code); 
+        
+        if(code.length < 6)
+            alert("Minimum length is 6");
+        else if(/^\d+$/.test(code) === false)
+            alert("Only numbers are allowed");
+    }
+
 
     return(
         <form onSubmit={addressSave}>
@@ -28,7 +68,8 @@ function AddressForm(props){
                         placeholder=' ' 
                         maxlength="10" 
                         required 
-                        className={classes.textBox}>
+                        className={classes.textBox}
+                        onChange = {mobHandler}>
                     </input>
                     <label className={classes.formLabel}>
                         Mobile No*
@@ -41,8 +82,10 @@ function AddressForm(props){
                     <input 
                         type= "text" 
                         placeholder=' ' 
+                        maxlength="6" 
                         required 
-                        className={classes.textBox}>
+                        className={classes.textBox}
+                        onBlur = {pinHandler}>
                     </input>
                     <label className={classes.formLabel}>
                         Pin Code*
