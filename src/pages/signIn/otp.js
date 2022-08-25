f6098f2ee026d26829dc8db83978df08325427ee
unknown <souhardya.dutta7801@gmail.com>
2022-08-25 16:04:02 +0530

    mark 33

--- src/pages/signIn/otp.js
+++ src/pages/signIn/otp.js
@@ -4,63 +4,72 @@ import classes from './otp.module.css';
 
 function Otp(props){
 
-    function validateNumber(evt) {
-        evt = (evt) ? evt : window.event;
-        var charCode = (evt.which) ? evt.which : evt.keyCode;
-        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-            return false;
-        }
-        return true;
+    function autoFocus(seq, final){
+        const initial = document.getElementById(seq);
+        if(initial.value.length)
+            document.getElementById(final).focus();
     }
     return(
         <div>
             <form>
-                <p>Enter OTP</p>
+                <p className={classes.head}>Verify with OTP</p>
+                <p className={classes.subHead}>Sent to xxxxxxxxxx</p>
                 <div className={classes.otpDetails}>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"first"}
                         className={classes.textBox}
-                        onkeypress={validateNumber}
+                        onKeyUp={() => autoFocus("first", 'sec')}
                         >
                     </input>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"sec"}
                         className={classes.textBox}
+                        onKeyUp={() => autoFocus("sec", 'third')}
                         >
                     </input>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"third"}
                         className={classes.textBox}
+                        onKeyUp={() => autoFocus("third", 'fourth')}
                         >
                     </input>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"fourth"}
                         className={classes.textBox}
+                        onKeyUp={() => autoFocus("fourth", 'fifth')}
                         >
                     </input>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"fifth"}
                         className={classes.textBox}
+                        onKeyUp={() => autoFocus("fifth", 'sixth')}
                         >
                     </input>
                     <input 
                         type= "text" 
                         maxLength= "1"
                         required 
+                        id= {"sixth"}
                         className={classes.textBox}
                         >
                     </input>
                 </div>
+                <p className={classes.resend}>RESEND OTP</p>
             </form>
         </div>
     );
