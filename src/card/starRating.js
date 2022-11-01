7a90471b5686959c12bf73bd663e0a23a8f63a5b
unknown <souhardya.dutta7801@gmail.com>
2022-11-01 08:29:02 +0530

    mark 80

--- src/card/starRating.js
+++ src/card/starRating.js
@@ -6,19 +6,7 @@ function StarRating(props) {
     const fullStar = props.stars;
     return(
         <div className={classes.rating}>
-            <span className={`${classes.star} ${fullStar >= 1 ? classes.starFill: ''}`}>
-                <StarIcon />
-            </span>
-            <span className={`${classes.star} ${fullStar >= 2 ? classes.starFill: ''}`}>
-                <StarIcon />
-            </span>
-            <span className={`${classes.star} ${fullStar >= 3 ? classes.starFill: ''}`}>
-                <StarIcon />
-            </span>
-            <span className={`${classes.star} ${fullStar >= 4 ? classes.starFill: ''}`}>
-                <StarIcon />
-            </span>
-            <span className={`${classes.star} ${fullStar === 5 ? classes.starFill: ''}`}>
+            <span className={`${classes.star} ${fullStar >= 4 ? classes.starFill: classes.avg}`}>
                 <StarIcon />
             </span>
         </div>
